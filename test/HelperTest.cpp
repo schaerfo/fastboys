219132e161248e8678a040a9e593b5b382c253d2
Christian Schärf <c.schaerf@t-online.de>
2020-11-05 21:07:23 +0100

    Add some tests.

---
+++ test/HelperTest.cpp
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2020 Christian Schärf
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <gtest/gtest.h>
+
+#include "Helpers.hpp"
+
+class HelpersTest: public ::testing::Test {
+protected:
+    void SetUp() override {
+        s = px = py = pz = BasisFunction({0, 0, 0}, {}, BasisFunction::Type::s);
+        px.type = BasisFunction::Type::px;
+        py.type = BasisFunction::Type::py;
+        pz.type = BasisFunction::Type::pz;
+    }
+
+    BasisFunction s, px, py, pz;
+};
+
+TEST_F(HelpersTest, is_s_orbital_test) {
+    EXPECT_TRUE(helpers::is_s_orbital(s.type));
+    EXPECT_FALSE(helpers::is_s_orbital(px.type));
+    EXPECT_FALSE(helpers::is_s_orbital(py.type));
+    EXPECT_FALSE(helpers::is_s_orbital(pz.type));
+}
+
+TEST_F(HelpersTest, is_p_orbital_test) {
+    EXPECT_FALSE(helpers::is_p_orbital(s.type));
+    EXPECT_TRUE(helpers::is_p_orbital(px.type));
+    EXPECT_TRUE(helpers::is_p_orbital(py.type));
+    EXPECT_TRUE(helpers::is_p_orbital(pz.type));
+}
